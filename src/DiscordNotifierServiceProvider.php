946808b95c112c2088a8f0dca7d90e445fb028c3
Arthur Parienté <arthur.pariente78960@gmail.com>
2026-07-13 13:37:24 +0200

    chore: personalize skeleton into arthurpar06/laravel-discord-notifier
    Rename the Spatie package-skeleton placeholders to the real package
    identity (namespace Arthurpar06\DiscordNotifier), rename config to
    discord-notifier.php, and drop the example command/facade/main-class,
    migration, and factory stubs the notifier does not need. Point the test
    harness at the renamed provider and scope PHPStan to src.

--- src/DiscordNotifierServiceProvider.php
+++ src/DiscordNotifierServiceProvider.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Arthurpar06\DiscordNotifier;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class DiscordNotifierServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +15,7 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_migration_table_name_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->name('discord-notifier')
+            ->hasConfigFile();
     }
 }
